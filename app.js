10acce195779f5384f046a3f0a7ac738ba3a33c6
Shweta Agraharo <sagrhari1994@gmail.com>
2020-06-30 22:12:28 +0530

    Inital Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+
+//database connection
+mongoose
+  .connect("mongodb://localhost:27017/tshirt", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+
+  //Middleware
+app.use(bodyParser.json());  
+app.use(cookieParser());
+app.use(cors());
+
+//routes
+//app.use("/api" , authRoutes);
+//port
+const port = process.env.PORT || 8000;
+
+
+//starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
+
+ 
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var router = express.Router();
+
+const { signout } = require("../controllers/auth");
+
+router.get("/signout", signout);
+
+module.exports = router;
+ 
